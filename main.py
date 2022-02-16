b74af9e43207c6b6a17e4b50c9865a63256965fc
itsgerliz <89922091+itsgerliz@users.noreply.github.com>
2022-02-16 09:46:03 +0100

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+#Importing libraries and modules
+import pygame
+from pygame.locals import *
+import sys
 
+#Constants
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+#Classes and functions
+
+
+
+#Main function
+def main():
+    pygame.init()
+    #Creating the window and setting the title
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Ping!Pong!")
+
+    #Main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
